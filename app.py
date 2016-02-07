858668a8c8e246c3e2e87928fa3bd153ae2bebdc
5parkp1ug <schandilya.abhi007@gmail.com>
2016-02-07 21:32:28 +0530

    Fixed erronous file

--- app.py
+++ app.py
@@ -1,40 +1,56 @@
 import jsontree
+path = 'testBookmarks'
 
+f = open(path)
 
-def getType(node):
-	if node['type'] == 'folder':
-		return 'folder'
-	elif node['type'] == 'url':
-		return 'url'
-	else:	
-		return -1
+data = jsontree.loads(f.read())
 
-def getInfo(node, *arg):
-	info = []
-	
-	if len(arg) > 1:
-		for i in arg:
-			info.append(node[i])
-	elif len(arg) == 1:	
-		return node[arg[0]]
-	return info
+testdata1 = data["roots"]["bookmark_bar"]
 
-		
-def getChildren(node):
-	for items in node["children"]:
-		if getType(items) == 'url':
-			print "URL- ", getInfo(items, 'url', 'date_added')
-		elif getType(items) == 'folder':
-			print "Folder Found: ",getInfo(items, 'name')
-			for each in items["children"]:
-				getChildren(each)
-	
+print type(testdata1["children"])
+'''
+for item in testdata1["children"]:
+	#print type(item)
+	type = item['type']
+	if type == 'folder':
+		print "its a folder with name : ",item['name']
+		print "the folder contents are - \n"
+		for each in item["children"]:
+			print each['type']
+'''
+#print testdata1
 
-		
-if __name__ == "__main__":
-	path = '/home/abhishek/Misc/Abhishek/scripts/BookMarkX/testBookmarks'
-	f = open(path)
-	data = jsontree.loads(f.read())
-	testdata1 = data["roots"]["bookmark_bar"]
-	
-	getChildren(testdata1)
+#print dir(testdata1["children"][0])
+
+#print jsontree.dumps(testdata1, indent=4)
+
+def parser(data, depth, path):
+	print "CURRENT PATH = ", path
+	for item in data["children"]:	# check for all the item in booknark bar
+		type = item['type']
+		if type == 'folder':      # if a folder is found do folder job
+			depth = depth + 1
+			path = path + item['name'] + '\\'
+			print depth*'-'+"Found a sub-folder with name : ",item['name']
+			print depth*' '+"-------------------------------"
+			size = len(item["children"])
+			print depth*'-'+"the size of folder is - ", size
+			for each in range(0,size):
+				if item["children"][each]["type"] == 'folder':
+					path = path + item["children"][each]["name"] + '\\'
+					
+					#parser(item["children"][each],depth, path)
+					parser(item["children"][each],depth, path)
+					print depth*'-'+"Name of Folder :- " , item["children"][each]["name"]
+					
+				elif item["children"][each]["type"] == 'url':
+					print depth*'-'+"URL :- " , item["children"][each]["url"]
+			
+		elif type == 'url':
+			depth = depth - 1
+			path = path + item['name'] + '\\'
+			print depth*'-'+"URL :- ", item['url']
+
+depth = 1	
+path = u'\\'		
+parser(testdata1, depth, path)
